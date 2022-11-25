ad41db95b84be0942ce4e5c0637eb6a330f15eb6
Hyeonjung1 <107379877+Hyeonjung1@users.noreply.github.com>
2022-11-26 00:58:25 +0900

    "refactor(Model):클래스 내에 필드값 generatedNumbers 추가"

--- src/main/java/baseball/domain/Model.java
+++ src/main/java/baseball/domain/Model.java
@@ -6,12 +6,17 @@ import java.util.List;
 public class Model {
 
     private static final int BALL_SIZE = 3;
+    private List<Integer> generatedNumbers;
 
-    public List<Integer> getStrikeBall (List<Integer> generatedNumber, List<Integer> usersNumber) {
+    public void setGenerateNumbers (List<Integer> generatedNumbers) {
+        this.generatedNumbers = generatedNumbers;
+    }
+
+    public List<Integer> getStrikeBall (List<Integer> usersNumber) {
         List<Integer> strikeBall = new ArrayList<>(List.of(0,0));
 
         for (int temp = 0; temp < BALL_SIZE; temp++) {
-            int check = checkStrikeBall(generatedNumber, usersNumber, temp);
+            int check = checkStrikeBall(usersNumber, temp);
 
             if(check == 1) strikeBall.set(0, strikeBall.get(0) +1);
             if(check == 2) strikeBall.set(1, strikeBall.get(1) +1);
@@ -20,11 +25,10 @@ public class Model {
         return strikeBall;
     }
 
-    private int checkStrikeBall (List<Integer> generatedNumber, List<Integer> usersNumber, int temp) {
+    private int checkStrikeBall (List<Integer> usersNumber, int temp) {
 
-        if(usersNumber.contains(generatedNumber.get(temp))) {
-            if(generatedNumber.get(temp).equals(usersNumber.get(temp))) return 1;
-
+        if(usersNumber.contains(generatedNumbers.get(temp))) {
+            if(generatedNumbers.get(temp).equals(usersNumber.get(temp))) return 1;
             return 2;
         }
         return 0;
