b24b25c4052a4d932205546a9c6c73d5ce5a9498
Hyeonjung1 <107379877+Hyeonjung1@users.noreply.github.com>
2022-11-26 00:06:23 +0900

    "feat(Model):볼과 스트라이크 갯수를 알아내는 메소드 수정"

--- src/main/java/baseball/domain/Model.java
+++ src/main/java/baseball/domain/Model.java
@@ -7,24 +7,26 @@ public class Model {
 
     private static final int BALL_SIZE = 3;
 
-    private List<Integer> checkStrike (List<Integer> generatedNumber, List<Integer> usersNumber) {
+    public List<Integer> getStrikeBall (List<Integer> generatedNumber, List<Integer> usersNumber) {
         List<Integer> strikeBall = new ArrayList<>(List.of(0,0));
 
         for (int temp = 0; temp < BALL_SIZE; temp++) {
-            if (generatedNumber.get(temp).equals(usersNumber.get(temp))) {
-                strikeBall.set(0, strikeBall.get(0) +1);
-                checkBall(generatedNumber, usersNumber, strikeBall);
-            }
+            int check = checkStrikeBall(generatedNumber, usersNumber, temp);
+
+            if(check == 1) strikeBall.set(0, strikeBall.get(0) +1);
+            if(check == 2) strikeBall.set(1, strikeBall.get(1) +1);
         }
-        strikeBall.set(1, strikeBall.get(1) - strikeBall.get(0));
 
         return strikeBall;
     }
 
-    private void checkBall (List<Integer> generatedNumber, List<Integer> usersNumber, List<Integer> strikeBall) {
+    private int checkStrikeBall (List<Integer> generatedNumber, List<Integer> usersNumber, int temp) {
 
-        if (generatedNumber.stream().anyMatch(usersNumber::contains)) {
-            strikeBall.set(1, strikeBall.get(1) +1);
+        if(usersNumber.contains(generatedNumber.get(temp))) {
+            if(generatedNumber.get(temp).equals(usersNumber.get(temp))) return 1;
+
+            return 2;
         }
+        return 0;
     }
 }
